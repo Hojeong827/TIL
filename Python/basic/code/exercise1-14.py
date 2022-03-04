a5a0dbfbdb3d1a470d3e88c51bc708e4380495d7
Hojeong <dbd03379@gmail.com>
2022-03-05 05:35:40 +0900

    exercise1-14.py 업데이트

---
+++ Python/basic/code/exercise1-14.py
@@ -0,0 +1,8 @@
+x1, y1, z1=1, 2, 3
+x2, y2, z2=3, 4, 5
+x3, y3, z3=x1+x2, y1+y2, z1+z2
+x4, y4, z4=x1-x2, y1-y2, z1-z2
+x5, y5, z5=x1*x2, y1*y2, z1*z2
+print(x3, y3, z3)
+print(x4, y4, z4)
+print(x5, y5, z5)
