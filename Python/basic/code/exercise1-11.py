2282b35dd64ae19cb2396477a766a416a99f7a30
Hojeong <dbd03379@gmail.com>
2022-03-05 05:35:21 +0900

    exercise1-11.py 업데이트

---
+++ Python/basic/code/exercise1-11.py
@@ -0,0 +1,10 @@
+score1=10
+score2=20
+score3=30
+n_student=3
+
+mean=(score1+score2+score3)/n_student
+square_of_mean=mean**2
+mean_of_square=(score1**2+score2**2+score3**2)/n_student
+print("square of mean: ", square_of_mean)
+print("mean of square: ", mean_of_square)
