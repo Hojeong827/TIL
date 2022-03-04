0c9d9fd249eccb5e187fcbf8d0cb9d3d91c240e3
Hojeong <dbd03379@gmail.com>
2022-03-05 05:54:03 +0900

    exercise1-20.py 업데이트

---
+++ Python/basic/code/exercise1-20.py
@@ -0,0 +1,8 @@
+pred1, pred2, pred3 = 10, 20, 30
+y1, y2, y3 = 10, 25, 40
+
+squared_error1=(pred1-y1)**2
+squared_error2=(pred2-y2)**2
+squared_error3=(pred3-y3)**2
+
+print(squared_error1, squared_error2, squared_error3)
