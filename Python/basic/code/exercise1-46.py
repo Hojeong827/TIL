10f24603e67658366053c57c4df34b8c88e8b25a
Hojeong <dbd03379@gmail.com>
2022-03-09 01:08:15 +0900

    exercise1-46.py 업데이트

---
+++ Python/basic/code/exercise1-46.py
@@ -0,0 +1,17 @@
+scores = [10, 20, 30]
+
+score_sum=0
+for score_idx in range(len(scores)):
+    score_sum += scores[score_idx]
+score_mean = score_sum / len(scores)
+
+# method.1
+score_ms=list()
+for score in scores:
+    score_ms.append(score-score_mean)
+print(score_ms)
+
+# method.2
+for score_idx in range(len(scores)):
+    scores[score_idx] -= score_mean
+print(score_ms)
