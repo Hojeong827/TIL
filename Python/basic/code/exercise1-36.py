844695f98049a8f706996a3fb9f41baec8558a1c
Hojeong <dbd03379@gmail.com>
2022-03-08 10:35:40 +0900

    exercise1-36.py 업데이트

---
+++ Python/basic/code/exercise1-36.py
@@ -0,0 +1,14 @@
+scores = [10, 20, 30]
+
+for score in scores: # scores 안에 있는 원소들에 대해서 하나씩 접근을 하고 이를 scroe 라는 변수에 할당을 한다.
+    print(score)
+    
+'''
+위의 반복문은 아래의 코드와 같다.
+score = scores[0]
+print(score)
+score = scores[1]
+print(score)
+score = scores[2]
+print(score)
+'''
