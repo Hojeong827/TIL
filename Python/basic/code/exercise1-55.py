fe601eea887b88c944f142e9bb3d92623bfb5223
Hojeong <dbd03379@gmail.com>
2022-03-10 02:24:53 +0900

    exercise1-55.py 업데이트

---
+++ Python/basic/code/exercise1-55.py
@@ -0,0 +1,15 @@
+v1=[1,2,3]
+square_sum=0
+for dim_val in v1:
+    square_sum+=dim_val**2
+norm=square_sum**0.5
+print("norm of v1: ", norm)
+
+for dim_idx in range(len(v1)):
+    v1[dim_idx]/=norm
+    
+square_sum=0
+for dim_val in v1:
+    square_sum+=dim_val**2
+norm=square_sum**0.5
+print("norm of v1: ", norm)
