060daa22196523ee55abe5ae38b6bc9471ade0f3
Hojeong <dbd03379@gmail.com>
2022-03-12 21:29:28 +0900

    exercise1-82.py 업데이트

---
+++ Python/basic/code/exercise1-82.py
@@ -0,0 +1,6 @@
+scores = [[10,20,30], [50, 60, 70]]
+print(scores)
+print(scores[0])
+print(scores[1])
+print(scores[0][0], scores[0][1], scores[0][2])
+print(scores[1][0], scores[1][1], scores[1][2])
