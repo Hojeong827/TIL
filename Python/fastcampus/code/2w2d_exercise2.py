7e967905c0a297af7f2cb8b11622f337794cd9a0
Hojeong <dbd03379@gmail.com>
2022-03-18 21:04:07 +0900

    2w2d_exercise2.py 업데이트

---
+++ Python/fastcampus/code/2w2d_exercise2.py
@@ -0,0 +1,15 @@
+# 에러 생성 : 10 이상의 숫자가 입력되도록 하는 에러
+# LowNumber
+
+class LowNumber(Exception):
+    
+    def __str__(self):
+        return "Number grater than 10"
+
+def input_number(num):
+    if num<=10:
+        raise LowNumber()
+    print(num)
+
+input_number(12)
+input_number(8)
