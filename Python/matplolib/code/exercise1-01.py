164a80130fd71f947e3e4498db03702d049649e3
Hojeong <dbd03379@gmail.com>
2022-03-27 13:49:14 +0900

    exercise1-01.py 업데이트

--- Python/matplolib/code/exercise1-01.py
+++ Python/matplolib/code/exercise1-01.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 fig = plt.figure(figsize=(7,7), facecolor='linen')
-ax1 = fig.add_subplot(221)
-ax2 = fig.add_subplot(222)
-ax3 = fig.add_subplot(212)
+ax1 = fig.add_subplot(221, facecolor='r')
+ax2 = fig.add_subplot(222, facecolor='g')
+ax3 = fig.add_subplot(212, facecolor='b')
 
 ax1.plot([2, 5, 10])
 ax2.plot([3, 9, 4])
