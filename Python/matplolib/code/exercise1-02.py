dd68fc9611146a265787449ad69d3773b460a6ae
HojeongPark <dbd03379@gamil.com>
2022-04-17 19:28:46 +0900

    exercise1-02.py 업데이트

--- Python/matplolib/code/exercise1-02.py
+++ Python/matplolib/code/exercise1-02.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots(figsize=(10,10))
-
 """
-xaxis = ax.get_xaxis()
+xaxis = ax.get_xaxis()              # 이 코드를 아래와 같이 간략하게 정리 가능
 yaxis = ax.get_yaxis()
 
 xaxis.set_visilbe(False)
